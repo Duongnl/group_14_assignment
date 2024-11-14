29f818f19338dee85d3d83a932538c8b02cfa9fa
Duongg <lengocduong003@gmail.com>
2024-11-14 07:23:26 +0700

    add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+import Image from "next/image"
+import { Form } from "react-bootstrap"
+import "@/styles/login.css"
+
 const HomePage = () => {
   return (
     <>
-        Đây là trang chủ
+                <div className='container-login__div'>
+            <Image className='background-image__Image'
+                src="/images/background-home.jpg"
+                alt="Picture of the author"
+                fill // Tự động điều chỉnh kích thước
+            />
+
+            <Form className="shadow p-4 bg-white rounded container-login__Form">
+                <div className="h4 text-center">Tổng hợp bài tập 10 tuần của Nhóm 14</div>
+            </Form>
+
+        </div>
     </>
   )
 }
